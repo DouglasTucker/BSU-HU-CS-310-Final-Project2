2483f332018b6a64382c3fad64e909c640f94b4a
DouglasTucker <DouglasTucker@u.boisestate.edu>
2021-02-20 21:05:17 -0700

    fixed SQL orders table creation

--- src/sql/create_orders_table.sql
+++ src/sql/create_orders_table.sql
@@ -4,5 +4,6 @@ order_id INT auto_increment,
 item_code varchar(10) NOT NULL,
 quantity int NOT NULL,
 order_timestamp timestamp DEFAULT CURRENT_TIMESTAMP,
+order_total double,
 primary key (order_id)
 );
